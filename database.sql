080ec9f3ad554386083b939e6efd3e9d71bd0fc6
ilhan-dahir <ahayad91@gmail.com>
2023-04-16 01:07:37 -0500

    added column to table for task completed

--- database.sql
+++ database.sql
@@ -1,16 +1,17 @@
 -- CREATE THE TABLE: 
 CREATE TABLE to_do_list (
 	"id" SERIAL PRIMARY KEY,
-	"item" VARCHAR(100)
+	"item" VARCHAR(100),
+    "completed" BOOL
 );
 
 -- Insert multiple rows into the koalas table:
 INSERT INTO koalas
 	("item")
 	VALUES
-	('Create to do list app'),
-    ('eat an apple'),
-	('get groceries');
+	('Create to do list app' 'false'),
+    ('eat an apple''false'),
+	('get groceries''false');
 	
 
 -- In postico, this command will show what is in your table currently:
