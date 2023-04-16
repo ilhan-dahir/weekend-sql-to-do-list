ed4abd154afd9003df32180cbd8018cb4d118fb0
ilhan-dahir <ahayad91@gmail.com>
2023-04-16 01:41:45 -0500

    render db items to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,5 +7,22 @@ $(document).ready(function () {
 
 
 function getItems() {
+    console.log('in getItems');
+    //ajax call to server to get items from db
+    $.ajax({
+        method: 'GET',
+        url: '/items'
+    }).then(function (response) {
+        //empty out items table
+        $('#view-items').empty();
 
-}
+        //loop through items form DB and render to DOM
+        for (let item of response) {
+            $('#view-items').append(
+                `
+            <tr data-id = ${item.id}>
+            <td>${item.item}</td>
+            `)
+        }
+    })
+}//end getItems
--- server/routes/items.router.js
+++ server/routes/items.router.js
@@ -6,6 +6,25 @@ const pool = require('../modules/pool.js');
 
 
 //GET
+itemRouter.get('/', (req, res) => {
+    console.log('GET /items');
+
+    let sqlText = `
+        SELECT * FROM "to_do_list"
+        ORDER BY "id";`;
+
+    pool.query(sqlText)
+        .then((dbRes) => {
+            let theItems = dbRes.rows;
+            res.send(theItems);
+        })
+        .catch((dbErr) => {
+            // Log that there was an issue with this function
+            console.log('SQL query in GET /items failed:', dbErr);
+            // Send "Internal Server Error" status to client
+            res.sendStatus(500)
+        })
+})
 
 //POST
 
